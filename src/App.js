ba80682933b2c63012d71a4cff05cbcadae5f569
NUOYANC <nuoyanc@student.unimelb.edu.au>
2021-09-02 03:34:49 +0800

    Update button

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import logo from './logo.svg';
 import agile from './agileSprite.png';
 
 import './App.css';
-import { Button } from '@material-ui/core';
+import {Button, ButtonGroup} from '@material-ui/core';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-
-        <p><strong>Hello world!! </strong></p>
-        ----***** AgileSprite *****---- <br/>
-        <a
-
-        >
-        </a>
-        <Button
-            color="primary"
-            className="App-link"
-            href="https://github.com/PinkPanther-ny/IT_project_0"
-            //target="_blank"
-            rel="noopener noreferrer"
-        >Check our github repo
-        </Button>
+        <p><strong>Welcome ! <br/>
+            Agile Sprite</strong></p>
 
         <a href="https://dashboard.heroku.com/apps/test0agilesprite">
           <img src={agile} className="Agile-logo" alt="logo" /></a>
-        <a href="https://dashboard.heroku.com/apps/test0agilesprite">
-          Go to our heroku console
-        </a>
 
-        <img id="agile-icon-position" src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
+        {/*<img id="agile-icon-position" src={logo} className="App-logo" alt="logo" />*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
+      <body className="App-body">
+      <ButtonGroup>
+        <Button
+            color="primary"
+            size="medium"
+            href="https://github.com/PinkPanther-ny/IT_project_0"
+            //target="_blank"
+            variant="contained"
+            rel="noopener noreferrer"
+        >Github repo
+        </Button>
+
+        <Button
+            color="secondary"
+            size="small"
+            href="https://dashboard.heroku.com/apps/test0agilesprite"
+            //target="_blank"
+            variant="contained"
+            rel="noopener noreferrer"
+        >Heroku console
+        </Button>
+      </ButtonGroup>
+      </body>
+
+
     </div>
   );
 }
