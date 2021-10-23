e379e7cd2b566fc59195bfdecc73592f15ab08db
NUOYANC <nuoyanc@student.unimelb.edu.au>
2021-10-24 05:48:37 +0800

    Remove invalid link.

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -5,7 +5,7 @@ function Logo(props) {
   const { children } = props;
 
   return (
-    <a href="https://dashboard.heroku.com/apps/test0agilesprite" target="_blank">
+    <a href="/">
       <div className="logo smart-layers-pointers">
         <div className="text-5 tahoma-bold-ebony-clay-35px">{children}</div>
       </div>
