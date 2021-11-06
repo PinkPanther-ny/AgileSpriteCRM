38315d2a32bc77b029be4d6a878afd513f2059f2
cameliaaaa <jieyic1@student.unimelb.edu.au>
2021-11-06 11:56:41 +0800

    add calendar birthday reminder

--- src/components/main/Calendar1.js
+++ src/components/main/Calendar1.js
@@ -3,6 +3,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./calendar.css";
 import styled from "styled-components";
+import cookie from "react-cookies";
+import { postDataToBackend } from "../../helper";
+import { CONTACT_GET_ALL_SUCCESS } from "../../backendReturnCodeHandling";
 const Button1 = styled.button`
   background: #3180e8;
   color: white;
@@ -12,38 +15,49 @@ const Button1 = styled.button`
   padding: 0.25em 10em;
   border-radius: 13px;
 `;
-const events = [
-  {
-    id: 1,
-    color: "#fd3153",
-    from:
-      localStorage.getItem("start date") +
-      "T" +
-      localStorage.getItem("start time") +
-      ":00+00:00",
-    to:
-      localStorage.getItem("start date") +
-      "T" +
-      localStorage.getItem("start time") +
-      ":00+00:00",
-    title: localStorage.getItem("content"),
-  },
-  {
-    id: 2,
-    color: "#1ccb9e",
-    from: "2019-05-01T13:00:00+00:00",
-    to: "2019-05-05T14:00:00+00:00",
-    title: "This is another event",
-  },
-  {
-    id: 3,
-    color: "#3694DF",
-    from: "2019-05-05T13:00:00+00:00",
-    to: "2019-05-05T20:00:00+00:00",
-    title: "This is also another event",
-  },
-];
 class Calendar1 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+      current: "",
+    };
+    this.loadAllBirthday();
+  }
+
+  loadAllBirthday() {
+    const token = { token: cookie.load("userToken") };
+    postDataToBackend("contact/get_all", token).then((responseJson) => {
+      if (responseJson["code"] === CONTACT_GET_ALL_SUCCESS) {
+        var arrayLength = responseJson["contacts"].length;
+
+        var currentYear = new Date().getFullYear();
+        for (var i = 0; i < arrayLength; i++) {
+          var from = new Date(
+            `${responseJson["contacts"][i]["birthday"]}T01:00:00+00:00`
+          );
+          this.state.events.push({
+            id: 1,
+            color: "#34d8eb",
+            from: `${currentYear}-${("0" + (from.getMonth() + 1)).slice(-2)}-${(
+              "0" + from.getUTCDay()
+            ).slice(-2)}T01:00:00+00:00`,
+            to: `${currentYear}-${("0" + (from.getMonth() + 1)).slice(-2)}-${(
+              "0" + from.getUTCDay()
+            ).slice(-2)}T24:00:00+00:00`,
+            title: `${responseJson["contacts"][i]["first_name"]} ${responseJson["contacts"][i]["last_name"]}
+              's birthday!`,
+          });
+        }
+        console.log(this.state.events);
+      } else {
+        // token error
+        alert(responseJson["msg"]);
+        window.location.href = "/login";
+      }
+    });
+  }
   render() {
     return (
       <div className="calendar">
@@ -52,12 +66,12 @@ class Calendar1 extends React.Component {
             <Button1>Add New Event!!</Button1>{" "}
           </Link>
         </div>
-        <Calendar className="wrapper" events={events} />
+        <Calendar className="wrapper" events={this.state.events} />
       </div>
 
-        // <React.Fragment>
-        //   <Calendar events={events} />
-        // </React.Fragment>
+      // <React.Fragment>
+      //   <Calendar events={events} />
+      // </React.Fragment>
     );
   }
 }
